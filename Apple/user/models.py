604f22c61885bb236a6c57a4b85f3be730af29ba
sourav <souravt0077@gmail.com>
2023-07-26 12:34:38 +0530

    some changes

--- Apple/user/models.py
+++ Apple/user/models.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email=models.EmailField(unique=True)
 
-    # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
