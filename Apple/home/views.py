8a0658d4e3ea05dcc71b0532eee6360a279acc95
sourav <souravt0077@gmail.com>
2023-07-26 13:13:39 +0530

    category,product models created

--- Apple/home/views.py
+++ Apple/home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from product.models import Category
 
 @login_required(login_url='login')
 def home(request):
-    context={}
+    categories=Category.objects.all()
+    context={'categories':categories}
     return render(request,'home.html',context)
--- Apple/product/models.py
+++ Apple/product/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    slug=models.SlugField()
+    name=models.CharField(max_length=200,blank=False,null=False)
+    category_image=models.ImageField(upload_to='Category_images')
+    created=models.DateTimeField(auto_now_add=True)
+    offer=models.BooleanField(default=False)
+    description=models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-created']
+
+class Products(models.Model):
+    slug=models.SlugField()
+    name=models.CharField(max_length=250,blank=False,null=False)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    product_image1=models.ImageField(upload_to='product_image1')
+    product_image2=models.ImageField(upload_to='product_image2')
+    product_image3=models.ImageField(upload_to='product_image3')
+    product_image4=models.ImageField(upload_to='product_image4')
+    product_image5=models.ImageField(upload_to='product_image5')
+    original_price=models.FloatField()
+    offer_price=models.FloatField()
+    description=models.TextField(max_length=500)
+    small_description = models.TextField(max_length=250)
+    offer=models.BooleanField(default=False)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-created']
