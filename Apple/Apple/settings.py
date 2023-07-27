4ed81d8d4ad64158000a209af521145f2a30adf0
sourav <souravt0077@gmail.com>
2023-07-27 10:59:06 +0530

    database changed to sqlite3 from mysql

--- Apple/Apple/settings.py
+++ Apple/Apple/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'Apple.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Apple',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-        'USER':'root',
-        'PASSWORD':''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
